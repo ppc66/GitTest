64c3f95fb2f9873fea1983a03521e5a0f05e66e3
wpc <wpc68@outlook.com>
2022-07-02 10:08:14 +0800

    v3 test.java

--- src/main/java/com/wpc/test.java
+++ src/main/java/com/wpc/test.java
@@ -2,6 +2,7 @@ package com.wpc;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git0");
+        System.out.println("hello git1");
     }
 }
