1fc9b609bee8788f9c43db3250835dc2140c186f
wpc <wpc68@outlook.com>
2022-07-02 13:53:14 +0800

    push commit

--- src/main/java/com/wpc/test.java
+++ src/main/java/com/wpc/test.java
@@ -7,5 +7,6 @@ public class test {
         System.out.println("hello git by hot-fix");
         System.out.println("hello git test by mast");
         System.out.println("hello git by hot-fix test");
+        System.out.println("hello git push test");
     }
 }
